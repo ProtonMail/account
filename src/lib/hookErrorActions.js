59c3aafcd1619a0a986949b0dfc80275bdb46f12
dhoko <aurelien@protonmail.com>
2018-06-19 11:52:08 +0200

    Add doc

--- src/lib/hookErrorActions.js
+++ src/lib/hookErrorActions.js
@@ -1,5 +1,13 @@
 const noop = () => {};
 
+/**
+ * Manage errors from actions
+ *  -- Mutate the store --
+ *  @link {https://github.com/developit/unistore/issues/83#issuecomment-389848046}
+ * @param  {Object} store
+ * @param  {Function} hook
+ * @return {void}
+ */
 export default function hookErrorActions(store, hook = noop) {
   const action = store.action;
   store.action = function(...args) {
