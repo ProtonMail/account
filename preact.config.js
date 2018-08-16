6b25e49a2df0cb6ebc1443b836caa1210fcab0ff
dhoko <aurelien@protonmail.com>
2018-08-16 15:47:58 +0200

    Fix build :hankey:

--- preact.config.js
+++ preact.config.js
@@ -1,6 +1,6 @@
 const preactCliLodash = require('preact-cli-lodash');
 
-module.exports = function (config) {
+module.exports = function (config, env, helpers) {
   config.node.process = 'mock';
   config.node.Buffer = true;
   // console.log(config);
@@ -10,4 +10,7 @@ module.exports = function (config) {
   const loaders = config.module.loaders;
   const babelLoader = loaders.find(loader => loader.loader === 'babel-loader');
   babelLoader.exclude = /node_modules/;
+
+  const { index } = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0];
+  config.plugins.splice(index, 1)
 };
